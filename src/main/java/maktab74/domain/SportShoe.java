5519190f5b8e54126a44f7c7269f9f7c470ecf3d
Hossein113 <hos_m18@yahoo.com>
2022-06-06 13:01:48 -0700

    update @Entiti

--- src/main/java/maktab74/domain/SportShoe.java
+++ src/main/java/maktab74/domain/SportShoe.java
@@ -1,10 +1,25 @@
 package maktab74.domain;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = SportShoe.TABLE_NAME)
 public class SportShoe extends Shoes {
 
+    public static final String TABLE_NAME = "sport_shoes";
+    public static final String STRA_TYPE = "stra_type";
+    public static final String SPORT_TYPE = "sport_type";
+
+    @Column(name = STRA_TYPE)
     private String StraType;
 
+    @Column(name = SPORT_TYPE)
     private String sportType;
+
 
     public SportShoe(int id, String name, String madeIn, int price, int number, int size, String material, String color, String straType, String sportType) {
         super(id, name, madeIn, price, number, size, material, color);
