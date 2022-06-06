7e6153f89b4db40cd357cde10b2a099b59799276
Hossein113 <hos_m18@yahoo.com>
2022-06-06 16:08:46 -0700

    added entityManager

--- src/main/java/maktab74/repositori/MagazineRepository.java
+++ src/main/java/maktab74/repositori/MagazineRepository.java
@@ -2,34 +2,21 @@ package maktab74.repositori;
 
 import maktab74.domain.Magazine;
 
+import javax.persistence.EntityManager;
 import java.sql.*;
 
 public class MagazineRepository {
 
-    private Connection connection;
+    private EntityManager entityManager;
 
-    public MagazineRepository(Connection db) {
-        this.connection = db;
+    public MagazineRepository(EntityManager em) {
+        this.entityManager = em;
     }
 
 
     public Magazine insertMagazine(Magazine magazine) throws SQLException {
 
-        String insertQuery = "insert into magazine_table (name , price , made_in ,title, page_type , page_size , number) " +
-                "values (?,?,?,?,?,?,?) ";
-        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-        preparedStatement.setString(1, magazine.getName());
-        preparedStatement.setInt(2, magazine.getPrice());
-        preparedStatement.setString(3, magazine.getMadeIn());
-        preparedStatement.setString(4, magazine.getTitle());
-        preparedStatement.setString(5, magazine.getPageType());
-        preparedStatement.setString(6, magazine.getSize());
-        preparedStatement.setInt(7, magazine.getNumber());
 
-
-        preparedStatement.executeUpdate();
-
-        magazine.setId(getMaxId());
         return magazine;
     }
 
