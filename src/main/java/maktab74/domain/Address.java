747aec2a4af3ca9e497199f4685d109b5abc7118
Hossein113 <hos_m18@yahoo.com>
2022-06-06 13:43:55 -0700

    added @entity

--- src/main/java/maktab74/domain/Address.java
+++ src/main/java/maktab74/domain/Address.java
@@ -1,15 +1,39 @@
 package maktab74.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = Address.TABLE_NAME)
 public class Address {
 
+
+    public static final String TABLE_NAME = "address_table";
+    public static final String ID = "id";
+    public static final String STATE = "state";
+    public static final String CITY = "city";
+    public static final String STREETS = "streets";
+    public static final String POSTALCODE = "PostalCode";
+
+    @Column(name =ID )
+    @GeneratedValue
     private int id;
 
+    @Column(name = STATE )
     private String State;
 
+
+    @Column(name =CITY )
     private String City;
 
+
+    @Column(name = STREETS)
     private String streets;
 
+
+    @Column(name = POSTALCODE)
     private String PostalCode;
 
     public Address() {
