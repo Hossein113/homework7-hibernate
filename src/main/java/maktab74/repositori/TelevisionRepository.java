a5d1cb25038f234330feca5098b5b13132e35131
Hossein113 <hos_m18@yahoo.com>
2022-06-06 16:06:14 -0700

    added entityManager

--- src/main/java/maktab74/repositori/TelevisionRepository.java
+++ src/main/java/maktab74/repositori/TelevisionRepository.java
@@ -2,40 +2,26 @@ package maktab74.repositori;
 
 import maktab74.domain.Television;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import java.sql.*;
 
 public class TelevisionRepository {
 
 
-    private Connection connection;
+    private EntityManager em ;
 
-    public TelevisionRepository(Connection connection) {
-        this.connection = connection;
+    public TelevisionRepository(EntityManager em) {
+        this.em = em;
     }
 
 
     public Television insertTelevision(Television television) throws SQLException {
 
-        String insertQuery = "insert into television_table ( name, price, made_in,color,inch_size,voltage_in, channel_number,speaker_number , weight," +
-                " image_type ,brand,number) values (?,?,?,?,?,?,?,?,?,?,?,?) ";
-        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-        preparedStatement.setString(1, television.getName());
-        preparedStatement.setInt(2, television.getPrice());
-        preparedStatement.setString(3, television.getMadeIn());
-        preparedStatement.setString(4, television.getColor());
-        preparedStatement.setInt(5, television.getInchSize());
-        preparedStatement.setString(6, television.getVoltageIn());
-        preparedStatement.setInt(7, television.getChannalNumber());
-        ;
-        preparedStatement.setInt(8, television.getSpeakerNumber());
-        preparedStatement.setString(9, television.getWeight());
-        preparedStatement.setString(10, television.getImageType());
-        preparedStatement.setString(11, television.getBrand());
-        preparedStatement.setInt(12, television.getNumber());
-
-        preparedStatement.executeUpdate();
-
-        television.setId(getMaxId());
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(television);
+        transaction.commit();
         return television;
     }
 
