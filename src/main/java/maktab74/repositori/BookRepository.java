997a996587ebbffd8561499222532ea2490fe285
Hossein113 <hos_m18@yahoo.com>
2022-06-06 16:06:03 -0700

    added entityManager

--- src/main/java/maktab74/repositori/BookRepository.java
+++ src/main/java/maktab74/repositori/BookRepository.java
@@ -1,37 +1,28 @@
 package maktab74.repositori;
 
 import maktab74.domain.Book;
+import maktab74.util.HibernateUtil;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import java.sql.*;
 
 public class BookRepository {
 
-    private Connection connection;
+    private EntityManager entityManager;
 
-    public BookRepository(Connection db) {
+    public BookRepository(EntityManager em) {
 
-        this.connection = db;
+        this.entityManager = em;
 
     }
 
     public Book insertBook(Book book) throws SQLException {
 
-        String insertQuery = "insert into book_table (name,price,made_in,title,pages_number,writer," +
-                "publisher,print_year,number) values (?,?,?,?,?,?,?,?,?) ";
-        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-        preparedStatement.setString(1, book.getName());
-        preparedStatement.setInt(2, book.getPrice());
-        preparedStatement.setString(3, book.getMadeIn());
-        preparedStatement.setString(4, book.getTitle());
-        preparedStatement.setInt(5, book.getPagesNumber());
-        preparedStatement.setString(6, book.getWriter());
-        preparedStatement.setString(7, book.getPublisher());
-        preparedStatement.setInt(8, book.getPrintYear());
-        preparedStatement.setInt(9, book.getNumber());
-
-        preparedStatement.executeUpdate();
-
-        book.setId(getMaxId());
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(book);
+        transaction.commit();
         return book;
     }
 
