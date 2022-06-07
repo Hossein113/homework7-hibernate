c17915c25975313dff7e25765193b8622af4a307
Hossein113 <hos_m18@yahoo.com>
2022-06-07 13:10:28 -0700

    add method

--- src/main/java/maktab74/repositori/SportShoeRepository.java
+++ src/main/java/maktab74/repositori/SportShoeRepository.java
@@ -4,7 +4,9 @@ import maktab74.domain.SportShoe;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import java.sql.*;
+import javax.persistence.TypedQuery;
+import java.sql.SQLException;
+import java.util.List;
 
 public class SportShoeRepository {
 
@@ -26,85 +28,37 @@ public class SportShoeRepository {
     }
 
     private int getMaxId() throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select max(id) from sportshoe_table");
-        if (resultSet.next()) {
-            return resultSet.getInt(1);
-        }
-        return 0;
+        TypedQuery query = entityManager.createQuery("select max(s) from SportShoe s", Integer.class);
+
+        return query.getFirstResult();
     }
 
-    public SportShoe[] getAllSportShoe() throws SQLException {
-        int allPrint = countAllSportShoe();
-
-
-        SportShoe[] sportShoes = new SportShoe[allPrint];
-        int count = 0;
-        String query = "select * from sportshoe_table";
-        PreparedStatement statement = connection.prepareStatement(query);
-        ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()) {
-
-            sportShoes[count] = new SportShoe(
-
-                    resultSet.getInt(1),
-                    resultSet.getString(2),
-                    resultSet.getString(7),
-                    resultSet.getInt(3),
-                    resultSet.getInt(10),
-                    resultSet.getInt(5),
-                    resultSet.getString(4),
-                    resultSet.getString(6),
-                    resultSet.getString(8),
-                    resultSet.getString(9)
-
-            );
-
-
-            count++;
-        }
-        return sportShoes;
+    public List<SportShoe> getAllSportShoe() throws SQLException {
+        TypedQuery<SportShoe> query = entityManager.createQuery("select s from SportShoe s ", SportShoe.class);
+        return query.getResultList();
     }
 
 
     public int countAllSportShoe() throws SQLException {
-        String query = "select count(*) from sportshoe_table";
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next()) {
-            return resultSet.getInt(1);
-        }
-        return 0;
+        TypedQuery query = entityManager.createQuery("select count(s)from SportShoe s", Integer.class);
+        int firstResult = query.getFirstResult();
+        return firstResult;
     }
 
     public SportShoe getById(int sportshoeId) throws SQLException {
-        String quri = "select * from sportshoe_table where id =" + sportshoeId;
-        PreparedStatement preparedStatement = connection.prepareStatement(quri);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next()) {
-            return new SportShoe(
-
-                    resultSet.getInt(1),
-                    resultSet.getString(2),
-                    resultSet.getString(7),
-                    resultSet.getInt(3),
-                    resultSet.getInt(10),
-                    resultSet.getInt(5),
-                    resultSet.getString(4),
-                    resultSet.getString(6),
-                    resultSet.getString(8),
-                    resultSet.getString(9)
-
-            );
-        }
-        return null;
+        return entityManager.find(SportShoe.class, sportshoeId);
     }
 
-    public void douwnProduct(int sportShoe) throws SQLException {
-        String quri = " update sportshoe_table set number=number-1 where id = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(quri);
-        preparedStatement.setInt(1, sportShoe);
-        preparedStatement.executeUpdate();
+    public void douwnProduct(int sportShoeId) throws SQLException {
+        entityManager.getTransaction().begin();
+
+        SportShoe sportShoe = entityManager.find(SportShoe.class, sportShoeId);
+
+        sportShoe.setNumber(sportShoe.getNumber() - 1);
+
+        entityManager.persist(sportShoe);
+
+        entityManager.getTransaction().commit();
     }
 
 }
