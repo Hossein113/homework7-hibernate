90c95695120b3894d6c4dd4a3fbacde5cc753163
Hossein113 <hos_m18@yahoo.com>
2022-06-07 13:10:35 -0700

    add method

--- src/main/java/maktab74/repositori/TelevisionRepository.java
+++ src/main/java/maktab74/repositori/TelevisionRepository.java
@@ -4,115 +4,61 @@ import maktab74.domain.Television;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import java.sql.*;
+import javax.persistence.TypedQuery;
+import java.sql.SQLException;
+import java.util.List;
 
 public class TelevisionRepository {
 
 
-    private EntityManager em ;
+    private EntityManager entityManager;
 
     public TelevisionRepository(EntityManager em) {
-        this.em = em;
+        this.entityManager = em;
     }
 
 
     public Television insertTelevision(Television television) throws SQLException {
 
-        EntityTransaction transaction = em.getTransaction();
+        EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
-        em.persist(television);
+        entityManager.persist(television);
         transaction.commit();
         return television;
     }
 
     private int getMaxId() throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select max(id) from television_table");
-        if (resultSet.next()) {
-            return resultSet.getInt(1);
-        }
-        return 0;
+        TypedQuery query = entityManager.createQuery("select max(t) from Television t ", Integer.class);
+
+        return query.getFirstResult();
     }
 
-    public Television[] getAllTelevision() throws SQLException {
-        int allPrint = countAllTelevision();
-
-
-        Television[] televisions = new Television[allPrint];
-        int count = 0;
-        String query = "select * from television_table";
-        PreparedStatement statement = connection.prepareStatement(query);
-        ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()) {
-
-            televisions[count] = new Television(
-
-                    resultSet.getInt(1),
-                    resultSet.getString(2),
-                    resultSet.getString(4),
-                    resultSet.getInt(3),
-                    resultSet.getInt(13),
-                    resultSet.getString(12),
-                    resultSet.getString(5),
-                    resultSet.getInt(6),
-                    resultSet.getString(7),
-                    resultSet.getInt(8),
-                    resultSet.getInt(9),
-                    resultSet.getString(10),
-                    resultSet.getString(11)
-
-            );
-
-
-            count++;
-        }
-        return televisions;
+    public List<Television> getAllTelevision() throws SQLException {
+        TypedQuery<Television> query = entityManager.createQuery("select t from Television t", Television.class);
+        return query.getResultList();
     }
 
 
     public int countAllTelevision() throws SQLException {
-        String query = "select count(*) from television_table";
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next()) {
-            return resultSet.getInt(1);
-        }
-        return 0;
+        TypedQuery query = entityManager.createQuery("select count(t)from Television t ", Integer.class);
+        int firstResult = query.getFirstResult();
+        return firstResult;
     }
 
     public Television getById(int televisionId) throws SQLException {
-        String quri = "select * from television_table where id =" + televisionId;
-        PreparedStatement preparedStatement = connection.prepareStatement(quri);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next()) {
-            return new Television(
-
-
-                    resultSet.getInt(1),
-                    resultSet.getString(2),
-                    resultSet.getString(4),
-                    resultSet.getInt(3),
-                    resultSet.getInt(13),
-                    resultSet.getString(12),
-                    resultSet.getString(5),
-                    resultSet.getInt(6),
-                    resultSet.getString(7),
-                    resultSet.getInt(8),
-                    resultSet.getInt(9),
-                    resultSet.getString(10),
-                    resultSet.getString(11)
-
-            );
-        }
-        return null;
+        return entityManager.find(Television.class, televisionId);
     }
 
-    public void douwnProduct(int television) throws SQLException {
-        String quri = " update television_table set number=number-1 where id = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(quri);
-        preparedStatement.setInt(1, television);
-        preparedStatement.executeUpdate();
-    }
+    public void douwnProduct(int televisionId) throws SQLException {
+        entityManager.getTransaction().begin();
 
+        Television television = entityManager.find(Television.class, televisionId);
+
+        television.setNumber(television.getNumber() - 1);
+
+        entityManager.persist(television);
+
+        entityManager.getTransaction().commit();
+    }
 
 }
