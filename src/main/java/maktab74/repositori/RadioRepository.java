ca6fdd4b927d344312f5c00873767428e735252c
Hossein113 <hos_m18@yahoo.com>
2022-06-07 12:23:50 -0700

    add method

--- src/main/java/maktab74/repositori/RadioRepository.java
+++ src/main/java/maktab74/repositori/RadioRepository.java
@@ -1,10 +1,13 @@
 package maktab74.repositori;
 
+import maktab74.domain.OfficalShoe;
 import maktab74.domain.Radio;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 import java.sql.*;
+import java.util.List;
 
 public class RadioRepository {
 
@@ -36,91 +39,40 @@ public class RadioRepository {
     }
 
     public int getMaxId() throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select max(id) from radio_table");
-        if (resultSet.next()) {
-            return resultSet.getInt(1);
-        }
-        return 0;
+        TypedQuery query = entityManager.createQuery("select max(r) from Radio r", Integer.class);
+
+        return query.getFirstResult();
+
     }
 
-    public Radio[] getAllRadio() throws SQLException {
-        int allPrint = countAllRadio();
-
-
-        Radio[] radios = new Radio[allPrint];
-        int count = 0;
-        String query = "select * from radio_table";
-        PreparedStatement statement = connection.prepareStatement(query);
-        ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()) {
-
-            radios[count] = new Radio(
-
-
-                    resultSet.getInt(1),
-                    resultSet.getString(2),
-                    resultSet.getString(4),
-                    resultSet.getInt(3),
-                    resultSet.getInt(12),
-                    resultSet.getString(11),
-                    resultSet.getString(5),
-                    resultSet.getString(6),
-                    resultSet.getString(7),
-                    resultSet.getInt(8),
-                    resultSet.getInt(9),
-                    resultSet.getString(10)
-
-            );
-
-
-            count++;
-        }
-        return radios;
+    public List<Radio> getAllRadio() throws SQLException {
+        TypedQuery<Radio> query = entityManager.createQuery("select r from Radio r", Radio.class);
+        return query.getResultList();
     }
 
-    public Radio getById(int RadioId) throws SQLException {
-        String quri = "select * from Radio_table where id =" + RadioId;
-        PreparedStatement preparedStatement = connection.prepareStatement(quri);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next()) {
-            return new Radio(
-
-
-                    resultSet.getInt(1),
-                    resultSet.getString(2),
-                    resultSet.getString(4),
-                    resultSet.getInt(3),
-                    resultSet.getInt(12),
-                    resultSet.getString(11),
-                    resultSet.getString(5),
-                    resultSet.getString(6),
-                    resultSet.getString(7),
-                    resultSet.getInt(8),
-                    resultSet.getInt(9),
-                    resultSet.getString(10)
-
-            );
-        }
-        return null;
+    public Radio getById(int radioId) throws SQLException {
+        return entityManager.find(Radio.class, radioId);
     }
 
 
     public int countAllRadio() throws SQLException {
-        String query = "select count(*) from radio_table";
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next()) {
-            return resultSet.getInt(1);
-        }
-        return 0;
+        TypedQuery query = entityManager.createQuery("select count(r)from Radio r", Integer.class);
+        int firstResult = query.getFirstResult();
+        return firstResult;
     }
 
     public void douwnProduct(int radioId) throws SQLException {
-        String quri = " update radio_table set number=number-1 where id = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(quri);
-        preparedStatement.setInt(1, radioId);
-        preparedStatement.executeUpdate();
+
+        entityManager.getTransaction().begin();
+
+        Radio radio = entityManager.find(Radio.class, radioId);
+
+        radio.setNumber(radio.getNumber() - 1);
+
+        entityManager.persist(radio);
+
+        entityManager.getTransaction().commit();
+
     }
 
 
